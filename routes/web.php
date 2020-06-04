5271fad7d2d8b86c81b19fc29a0f6977e1ca3fc6
wahid09 <wahid.ullah.cse@gmail.com>
2020-06-04 16:38:28 +0600

    API integrate and data load from TMDB

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
-
-Route::view('/', 'index');
-Route::view('/movie', 'show');
+Route::get('', 'MoviesController@index')->name('movies.index');
+Route::get('/movies/{movie}', 'MoviesController@show')->name('movies.show');
