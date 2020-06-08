32506a92005197a9c15ac600e160561fbc274621
wahid09 <wahid.ullah.cse@gmail.com>
2020-06-08 09:22:16 +0600

    search created by lovewire

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class SearchDropdown extends Component
 {
+	public $search = '';
     public function render()
     {
-        return view('livewire.search-dropdown');
+    	$searchResult = [];
+    	if(strlen($this->search) > 2){
+    		$searchResult = Http::withToken(config('services.tmdb.token'))
+                       ->get('https://api.themoviedb.org/3/search/movie?query='.$this->search)
+                       ->json()['results'];
+    	}
+        //dump($searchResult);
+        return view('livewire.search-dropdown', [
+        	'searchResult'=> collect($searchResult)->take(10),
+        ]);
     }
 }
